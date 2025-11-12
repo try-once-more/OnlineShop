7589f03cca3721150da356af21c1db512efcc6ed
Vadzim Zianiuk <81385450+try-once-more@users.noreply.github.com>
2025-11-12 20:29:15 +0100

    Added CartService.API

---
+++ src/CartService.API/ApiVersions.cs
@@ -0,0 +1,9 @@
+﻿namespace CartService.API;
+
+public class ApiVersions
+{
+    public const string V1 = "v1";
+    public const string V2 = "v2";
+
+    public static readonly IReadOnlyList<string> All = [V1, V2];
+}
---
+++ src/CartService.API/Program.cs
@@ -0,0 +1,98 @@
+using CartService.API;
+using CartService.API.Endpoints;
+using CartService.API.Middlewares;
+using CartService.Application.Entities;
+using Mapster;
+using MapsterMapper;
+using System.Reflection;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+    {
+        string key = httpContext.User.Identity?.Name
+            ?? httpContext.Connection.RemoteIpAddress?.ToString()
+            ?? string.Empty;
+
+        return RateLimitPartition.GetFixedWindowLimiter(key, _ => new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = 100,
+            Window = TimeSpan.FromMinutes(1),
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+            QueueLimit = 0
+        });
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    foreach (var version in ApiVersions.All)
+    {
+        options.SwaggerDoc(version, new() { Title = "Cart API", Version = version });
+    }
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+        options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddLogging();
+builder.Services.AddMetrics();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddSingleton<ErrorHandlingMiddleware>();
+
+builder.Services.Configure<CartDatabaseSettings>(builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddCartServiceInfrastructure();
+builder.Services.AddCartServiceApplication();
+AddMapper(builder.Services);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseRateLimiter();
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseHealthChecks("/health");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(o => o.RouteTemplate = "openapi/{documentName}.json");
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var version in ApiVersions.All)
+        {
+            options.SwaggerEndpoint($"/openapi/{version}.json", $"Cart API {version}");
+        }
+    });
+}
+
+app.MapCartEndpointsV1();
+app.MapCartEndpointsV2();
+
+app.Run();
+
+static void AddMapper(IServiceCollection services)
+{
+    var config = TypeAdapterConfig.GlobalSettings;
+    config.NewConfig<CartItem, CartItemResponse>();
+    config.NewConfig<ImageInfo, ImageInfoResponse>();
+
+    services.AddSingleton<IMapper>(new Mapper(config));
+}
+
+
+public partial class Program { }

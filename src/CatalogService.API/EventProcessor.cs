1074d9e5dad1975c483afe3dc265410b7204eb54
Vadzim Zianiuk <81385450+try-once-more@users.noreply.github.com>
2025-11-29 09:04:57 +0100

    fixed EventProcessor

--- src/CatalogService.API/EventProcessor.cs
+++ src/CatalogService.API/EventProcessor.cs
@@ -1,6 +1,6 @@
 ﻿using CatalogService.Application.Abstractions;
 
-internal sealed class EventProcessor(IServiceProvider services, ILogger<EventProcessor> logger) : BackgroundService
+internal sealed class EventProcessor(IServiceScopeFactory scopeFactory, ILogger<EventProcessor> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
@@ -8,7 +8,7 @@ internal sealed class EventProcessor(IServiceProvider services, ILogger<EventPro
         {
             try
             {
-                using var scope = services.CreateScope();
+                using var scope = scopeFactory.CreateScope();
                 var processor = scope.ServiceProvider.GetRequiredService<IEventPublisherService>();
                 await processor.PublishPendingAsync(cancellationToken: cancellationToken);
             }

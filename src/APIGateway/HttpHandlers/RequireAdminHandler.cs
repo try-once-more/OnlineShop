29b9419eb80f91a8c9dbb71448fb2d46f684ac76
Vadzim Zianiuk <81385450+try-once-more@users.noreply.github.com>
2025-12-28 14:56:14 +0100

    Added APIGateway

---
+++ src/APIGateway/HttpHandlers/RequireAdminHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using APIGateway.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace APIGateway.HttpHandlers;
+
+internal class RequireAdminHandler(IOptions<AdminRole> adminRole) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request?.Headers, nameof(request.Headers));
+        var auth = request.Headers.Authorization;
+        if (auth?.Scheme != "Bearer" || string.IsNullOrEmpty(auth.Parameter))
+        {
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
+
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(auth.Parameter);
+        var claims = jwt.Claims.ToImmutableHashSet();
+        if (!adminRole.Value.RequiredClaims.All(rc => claims.Any(c => c.Type == rc.Type && c.Value == rc.Value)))
+        {
+            return new HttpResponseMessage(HttpStatusCode.Forbidden);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
---
+++ src/APIGateway/Program.cs
@@ -0,0 +1,41 @@
+﻿using APIGateway.Aggregators;
+using APIGateway.Configuration;
+using APIGateway.HttpHandlers;
+using APIGateway.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Remove("scp");
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Add("scp", "scope");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<JwtAuthOptions>(builder.Configuration.GetSection("Authentication"));
+builder.Services.Configure<AdminRole>(builder.Configuration.GetSection("AdminRole"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
+builder.Services.AddSingleton<ErrorHandlingMiddleware>();
+
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(x => x.WithDictionaryHandle())
+    .AddDelegatingHandler<RequireAdminHandler>()
+    .AddTransientDefinedAggregator<RouteKeysAggregator>();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration, o =>
+{
+    // Does not work currently as Swagger cannot retrieve OpenAPI documentation for aggregate routes
+    o.GenerateDocsForAggregates = true;
+});
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseSwaggerForOcelotUI();
+
+await app.UseOcelot();
+await app.RunAsync();
